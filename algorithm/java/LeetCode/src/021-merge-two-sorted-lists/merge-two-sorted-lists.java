4eecbecd6ee77ddf5e0411030f79104d233b1dc5
qingliu <shuiqingliu14@gmail.com>
2019-03-05 14:04:27 +0800

    refactor the merge method of sorted list

--- algorithm/java/LeetCode/src/021-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ algorithm/java/LeetCode/src/021-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,13 +1,3 @@
-// Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
-//
-// Example:
-//
-// Input: 1->2->4, 1->3->4
-// Output: 1->1->2->3->4->4
-//
-//
-
-
 /*
  * @lc app=leetcode id=21 lang=java
  *
@@ -42,38 +32,26 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode result = new ListNode(0);
-        ListNode currentNode = new ListNode(0);
-        result = currentNode;
-        while(l1 != null){
-            if(l2 != null){
+        ListNode currentNode = result;
+        //截取到都不为空 剩下的直接拼接
+        while(l1 != null && l2 != null){
                 if(l1.val <= l2.val){
-                    ListNode tempNode = l1;
-                    currentNode.next = tempNode;
-                    currentNode = tempNode;
+                    currentNode.next = l1;
                     l1 = l1.next;
                 }else if(l1.val > l2.val){
-                    ListNode tempNode = l2;
-                    currentNode.next = tempNode;
-                    currentNode = tempNode;
+                    currentNode.next = l2;
                     l2 = l2.next;
                 }
-            }else{
-                ListNode tempNode = l1;
-                currentNode.next = tempNode;
-                currentNode = tempNode;
-                l1 = l1.next;
-            }
+                currentNode = currentNode.next;
+        }
+        //直接拼接
+        if(l2 != null){
+            currentNode.next = l2;
+        }else{
+            currentNode.next = l1;
         }
 
-        if(l2 != null){
-            ListNode tempNode = l2;
-            currentNode.next = tempNode;
-            currentNode = tempNode;
-            l2 = l2.next;
-        }
         return result.next;
-        
     }
 }
-
 
